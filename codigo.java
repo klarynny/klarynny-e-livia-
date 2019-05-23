cf94cd0ccf5548c471ff187c0211d4efaf018c1a
klarynny <49276346+klarynny@users.noreply.github.com>
2019-05-23 10:22:16 -0300

    codigo em java
    codigo convertido

---
+++ codigo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class DemoTranslation {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i + " par");
+			} else {
+				System.out.println(i + " impar");
+			}
+		}
+	}
+}
