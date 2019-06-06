0b802d8a9ec6f1b70fbb0f2a9d95c8b725d34cb2
Lívia Henrique <49006944+liviahenrique100@users.noreply.github.com>
2019-06-06 10:28:16 -0300

    codigo do parque
    parque.java parte 2

---
+++ parque.java
@@ -0,0 +1,21 @@
+package com.br.iffiana.klarynnyelivinha;
+
+/**
+ *
+ * @author aluno
+ */
+public class Parque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Gato puppy= new Gato("floco");
+        puppy.nome="jujuba";
+        puppy.miado();
+        Gato femea= new Gato("amora");
+        femea.idade=2;
+        femea.miado();
+    }
+        // TODO code application logic here
+    }
