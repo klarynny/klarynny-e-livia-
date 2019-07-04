df0e876043c28c5698ecd50aceaff782a578c0fb
klarynny <49276346+klarynny@users.noreply.github.com>
2019-07-04 09:59:24 -0300

    zattini.java

---
+++ preenchido.java
@@ -0,0 +1,19 @@
+package Zattini;
+
+public class Cliente {
+	zattini.nome= "Klarynny Oliveira";
+	zattini.data de nascimento= "07/11/2002";
+	zattini.cpf= "153.498.807-64";
+	zattini.telefone= "22992913150";
+}
+
+public class Endereco {
+	zattini.nome da rua="avenida francisco de assis carneiro da silva";
+	zattini.numero da casa="07";
+	zattini.complemento="casa";
+	zattini.bairro="caxias";
+	zattini.cep="28735000";
+	zattini.cidade="Quissamã";
+	zattini.estado="Rio de Janeiro";
+}
+
---
+++ zantinni-cliente-endereco.java
@@ -0,0 +1,22 @@
+package com.br.iffiana.klarynnyelivinha;
+
+package Zattini;
+
+public class Cliente { 
+   String nome;
+   String data de nascimento;
+   String cpf;
+   String telefone;
+
+
+}
+
+public class Endereco {
+  String nome da rua;
+  int numero da casa;
+  String complemento;
+  String bairro;
+  String cep;
+  String cidade;
+  String estado;
+}            
